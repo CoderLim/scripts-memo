122bb85527f1e8e8f04f5f87315cdfc6477ab1a0
耿笠茗 <gengliming@le.com>
2018-11-02 16:16:44 +0800

    bugfixed

--- login_jumper.py
+++ login_jumper.py
@@ -17,21 +17,25 @@ def normalize_winsize (child):
     def sigwinch_callback (sig, data):
         s = struct.pack("HHHH", 0, 0, 0, 0)
         a = struct.unpack('hhhh', fcntl.ioctl(sys.stdout.fileno(), termios.TIOCGWINSZ , s))
-        child.setwinsize(a[0], a[1])
+        if not child.closed:
+            child.setwinsize(a[0], a[1])
 
-    # 当收到窗口尺寸变化时调用
+    # 当收到窗口尺寸变化信号时调用
     signal.signal(signal.SIGWINCH, sigwinch_callback)
     # 手动触发SIGWINCH信号
     # 因为默认不会有信号发出
-    os.kill(os.getpid(), signal.SIGWINCH);
+    os.kill(os.getpid(), signal.SIGWINCH)
 
 def main():
     userpass = os.environ['LUS']
     username = os.environ['LUN']
-    jump_host = '10.112.41.59'
-    jump_port = '60022'
+
+    # 堡垒机
     fort_host = 'jump3.oss.letv.cn'
     fort_port = '50022'
+    # 跳板机
+    jump_host = '10.112.41.59'
+    jump_port = '60022'
 
     # 登陆堡垒机
     fort_cmd = "ssh {username}@{host} -p {port}".format(username=username, host=fort_host, port=fort_port)
@@ -65,7 +69,7 @@ def main():
     fortress.sendline(jump_cmd)
 
     try:
-        fortress.expect("{username}@{host}:{port}'s password:".format(username=username, host=jump_host, port=jump_port))
+        fortress.expect([username, jump_host, "{username}@{host}:{port}'s password:".format(username=username, host=jump_host, port=jump_port)])
     except Exception as e:
         print(fortress.before)
         print("++3++")
